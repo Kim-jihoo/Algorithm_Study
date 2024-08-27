52beaf542a05546f4de242b96db8266e076afad2
Kim-jihoo <109203288+Kim-jihoo@users.noreply.github.com>
2024-08-27 16:37:35 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 568 ms, Memory: 296464 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        
+        Queue<Integer> q = new LinkedList<>();
+        
+        for(int i=1; i<=n; i++){
+            q.offer(i);
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        
+        while(q.size()!=1){
+            for(int i=0; i< k-1; i++){
+                q.offer(q.poll());
+            }
+            sb.append(q.poll()+", ");
+        }
+            sb.append(q.poll()+">");
+            bw.write(sb.toString() +"\n");
+            bw.flush();
+            bw.close();
+            br.close();
+    }
+}
