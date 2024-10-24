2e01f51f5097c73b2b2fdaf850fe574cc60b4265
Kim-jihoo <109203288+Kim-jihoo@users.noreply.github.com>
2024-10-24 13:26:16 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 100 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,14 +1,14 @@
 import java.io.*;
+import java.util.*;
 public class Main{
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        String[] a = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
-
-        for(int i=0; i<a.length;i++){
-            String ch = a[i];
-            str = str.replace(ch, "*");
+        String [] str = {"c=","c-","dz=","d-","lj","nj","s=","z="};
+        String a = br.readLine();
+        for(int i=0; i<str.length; i++){
+            String ch = str[i];
+            a = a.replace(ch,"*");
         }
-        System.out.println(str.length());
+        System.out.println(a.length());
     }
 }
