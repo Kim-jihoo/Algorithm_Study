a33409cd638134eefd6330b5add90a4cde1ec495
Kim-jihoo <109203288+Kim-jihoo@users.noreply.github.com>
2024-10-24 21:02:19 +0900

    [Silver V] Title: 나이순 정렬, Time: 1248 ms, Memory: 55388 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,26 +1,36 @@
 import java.io.*;
 import java.util.*;
+
+class Member{
+    int age;
+    String name;
+    public Member(int age, String name){
+        this.age = age;
+        this.name=name;
+    }
+}
+
 public class Main{
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        String [][] arr = new String[n][2];
+        
+        Member[] members = new Member [n];
         for(int i=0; i<n; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine()); 
-            arr[i][0] = st.nextToken();
-            arr[i][1] = st.nextToken();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int age = Integer.parseInt(st.nextToken());
+            String name = st.nextToken();
+            members[i] = new Member(age, name);
         }
-       Arrays.sort(arr,(a,b)->{
-           int a1 = Integer.parseInt(a[0]);
-           int b1 = Integer.parseInt(b[0]);
-           if(a1!=b1){
-               return a1-b1;
-           }else{
-               return 0;
-           }
-       });
-        for(int i=0; i<n; i++){
-            System.out.println(arr[i][0] + " " + arr[i][1]);
-        }
+        Arrays.sort(members,(a,b)->{
+            if(a.age!=b.age){
+                return a.age-b.age;
+            }else{
+                return 0;
+            }
+        });
+     for(Member member : members){
+         System.out.println(member.age+" "+ member.name);
+     }
     }
 }
