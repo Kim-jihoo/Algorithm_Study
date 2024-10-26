93732859932384a4998432fa309f0065410eff3d
Kim-jihoo <109203288+Kim-jihoo@users.noreply.github.com>
2024-10-26 11:16:02 +0900

    [level 0] Title: 다항식 더하기, Time: 11.98 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.java
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.java
@@ -1,37 +1,36 @@
+import java.util.*;
 class Solution {
     public String solution(String polynomial) {
         String answer = "";
-        int a=0; int b=0;
-        
-       
-            String [] str = polynomial.split(" ");
-            for(int j=0; j<str.length; j++){
-                if(str[j].endsWith("x")){
-                    if(str[j].equals("x")){
-                        a+=1;
-                    }else{
-                        a += Integer.parseInt(str[j].substring(0,str[j].length()-1));
-                    }
-                 
-                }else if(str[j].equals("+")){
-                    continue;
-                }else{
-                     b += Integer.parseInt(str[j]);
-                }
+        int n = 0;
+        int m = 0;
+        String [] str = polynomial.split(" ");
+        for(int i=0; i<str.length; i++){
+            if(str[i].contains("x")&&str[i].length()>1){
+                n= n+Integer.parseInt(str[i].substring(0,str[i].length()-1));
+            }else if(str[i].equals("x")){
+                n++;
+            }else if(str[i].equals("+")){
+                continue;
+            }else{
+                m=m+Integer.parseInt(str[i]);
             }
-        
-        if(a>1&&b>0){
-            answer = a+"x"+" + "+b;
-        }else if(a>1){
-            answer = a+"x";
-        }else if(a==1&&b>0){
-            answer="x"+" + "+b;
-        }else if(a==1&&b==0){
-            answer="x";
-        } else{
-            answer = String.valueOf(b);
         }
-        
+        if(n>1){
+            if(m>=1){
+                answer = String.valueOf(n)+"x"+ " + "+String.valueOf(m);
+            }else{
+                answer = String.valueOf(n)+"x";
+            } 
+        }else if(n==0){
+            answer = String.valueOf(m);
+        }else if(n==1){
+            if(m>=1){
+                answer = "x"+ " + "+String.valueOf(m);
+            }else{
+                answer = "x";
+            }
+        }
         return answer;
     }
 }
