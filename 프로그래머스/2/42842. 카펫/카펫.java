4438da4589465d023498ecd6f82d9bfbbdf37021
Kim-jihoo <109203288+Kim-jihoo@users.noreply.github.com>
2024-10-27 00:48:54 +0900

    [level 2] Title: 카펫, Time: 0.12 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -2,16 +2,7 @@ import java.util.*;
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        /*  
-            y 1줄 -> b=2+2*(y+2) // y+2,3
-            y n줄(짝) -> b=2*((y/n)+2)+2*n // y/n+2,n+2
-            y n줄(홀) -> b=(y/n+2)*(n+2)-y //y/n+2, n+2
-            b+y=(y/n+2)(n+2);
-            00000
-            00000
-            00000
-            00000
-        */
+/*
         int a = brown+yellow;
         
         for(int i=1; i<a; i++){
@@ -24,6 +15,16 @@ class Solution {
             int temp=answer[1];
             answer[1]=answer[0];
             answer[0]=temp;
+        }*/
+        int num = brown+yellow;
+        for(int i=1; i<brown;i++){
+            int a = yellow/i;
+            int sum = (a+2)*(i+2);
+            if(sum==num&&yellow%i==0){
+                answer[0]=Math.max(a+2,i+2);
+                answer[1]=Math.min(a+2,i+2);
+                break;
+            }
         }
         return answer;
     }
