85adbb8cd6dfbd04147824336b4b9e40b6477f14
Kim-jihoo <109203288+Kim-jihoo@users.noreply.github.com>
2024-10-26 10:30:35 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.75 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -2,18 +2,30 @@ import java.util.*;
 class Solution {
     public int solution(int[][] lines) {
         int answer = 0;
-       
-        int[] interval = new int[201];
-        for(int[] line : lines){
-            int start = line[0]+100;
-            int end = line[1]+100;
-            for(int i=start; i<end; i++){
-                interval[i]++;
-            }
+        for(int i=0; i<3; i++){
+            lines[i][0]+=100;
+            lines[i][1]+=100;
         }
-        for(int i=0; i<201; i++){
-            if(interval[i]>=2){
-                answer++;
+        Arrays.sort(lines,(a,b)->{
+            if(a[0]!=b[0]){
+                return a[0]-b[0];
+            }else{
+                return a[1]-b[1];
+            }
+        });
+        int start = lines[0][0];
+        int end = lines[0][1];
+        int length = 0;
+        for(int i=1; i<3; i++){
+            if(end>lines[i][0]){
+                answer +=Math.min(end,lines[i][1])-Math.max(lines[i][0], start);
+                int a = Math.max(end,lines[i][0]);
+                int b = lines[i][1];
+                start = Math.min(a,b);
+                end = Math.max(a,b);
+            }else{
+                start = lines[i][0];
+                end = lines[i][1];
             }
         }
         return answer;
