884e90e0b0a46556bf4ce001196a28343590a526
Kim-jihoo <109203288+Kim-jihoo@users.noreply.github.com>
2024-10-27 00:30:41 +0900

    [level 2] Title: 소수 찾기, Time: 41.60 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,45 +1,45 @@
 import java.util.*;
 class Solution {
-    public static Set<Integer> set;
-    public static boolean[] visit;
+    public static HashSet<Integer> set;
+    public static boolean visited[];
     public int solution(String numbers) {
         int answer = 0;
-        set = new HashSet<>();
-        visit = new boolean[numbers.length()];
-       
-          dfs(numbers,"",0);
+        set=new HashSet<>();
+        visited=new boolean[numbers.length()];
+        dfs(numbers,"",0);
         for(int key: set){
             if(isPrime(key)){
                 answer++;
             }
-        };
+        }
         return answer;
     }
-    public static void dfs(String numbers, String str, int depth){
-        if(!str.isEmpty()){
-            set.add(Integer.parseInt(str));
+    public void dfs(String numbers, String s, int depth){
+        if(!s.isEmpty()){
+            set.add(Integer.parseInt(s));
         }
         if(depth==numbers.length()){
             return;
         }
-        String[] s = numbers.split("");
-        for(int i=0; i<s.length;i++){
-            if(!visit[i]){
-                visit[i]=true;
-                dfs(numbers,str+s[i],depth+1);
-                visit[i]=false;
+        
+        String [] str =numbers.split("");
+        for(int i=0; i<str.length;i++){
+            if(!visited[i]){
+                visited[i]=true;
+                dfs(numbers,s+str[i],depth+1);
+                visited[i]=false;
             }
         }
     }
-    public static boolean isPrime(int n){
+    public boolean isPrime(int n){
         if(n<2){
             return false;
-        }
-        for(int i=2; i<=Math.sqrt(n);i++){
+        }else{
+            for(int i=2; i<=Math.sqrt(n);i++){
             if(n%i==0){
                 return false;
             }
-        }
+        }}
         return true;
-    };
+    }
 }
