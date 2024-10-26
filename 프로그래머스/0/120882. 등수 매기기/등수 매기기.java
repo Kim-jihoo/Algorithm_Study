f6145181af13227330dc4f761d92b3c91eb744c5
Kim-jihoo <109203288+Kim-jihoo@users.noreply.github.com>
2024-10-26 11:45:17 +0900

    [level 0] Title: 등수 매기기, Time: 0.03 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
@@ -1,24 +1,21 @@
-import java.util.*;
 class Solution {
     public int[] solution(int[][] score) {
         
+        double[] avg = new double[score.length];
         int[] answer = new int[score.length];
-        Double [][] n = new Double[score.length][2];
-        
-        for(int i=0; i<score.length;i++){
-            Double average = (score[i][0]+score[i][1])/2.0;
-            n[i][0] = average;
-            n[i][1]=(double)i;
-        }
-        Arrays.sort(n,(a,b)->b[0].compareTo(a[0]));
 
-        int rank =1;
-        for(int i=0; i<score.length; i++){
-            if(i>0&&!n[i][0].equals(n[i-1][0])){
-                rank=i+1;
+        for(int i=0; i<score.length; i++) {
+            avg[i] = (double)(score[i][0] + score[i][1]) / 2;
+        }
+
+        for(int i=0; i<avg.length; i++) {
+            int rank = 1;
+            for(int j=0; j<avg.length; j++) {
+                if(avg[i] < avg[j]) {
+                    rank++;
+                }
             }
-            
-            answer[(int)(n[i][1]).doubleValue()] = rank;
+            answer[i] = rank;
         }
         return answer;
     }
