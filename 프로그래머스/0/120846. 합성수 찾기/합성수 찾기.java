0146e95a3f91de9d2eb5dff2096c40c37f126627
Kim-jihoo <109203288+Kim-jihoo@users.noreply.github.com>
2024-11-18 23:17:07 +0900

    [level 0] Title: 합성수 찾기, Time: 0.10 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i=3; i<=n; i++){
+            int num=1;
+            for(int j=2; j<=i; j++){
+                if(i%j==0) num++;
+            }
+            if(num>2) answer++;
+        }
+        return answer;
+    }
+}
