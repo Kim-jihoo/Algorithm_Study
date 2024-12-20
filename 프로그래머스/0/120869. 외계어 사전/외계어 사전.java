cf97a744b9eb048312e24a411d64e3bc330e8aa3
Kim-jihoo <109203288+Kim-jihoo@users.noreply.github.com>
2024-12-20 23:33:34 +0900

    [level 0] Title: 외계어 사전, Time: 1.58 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
@@ -1,20 +1,22 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(String[] spell, String[] dic) {
-        int answer = 2;
-        for(int i=0; i<dic.length; i++){
-            boolean isMatch=true;
-            
-            for(int j=0; j<spell.length; j++){
-                if(!dic[i].contains(spell[j])){
-                    isMatch=false;
-                    break;
-                }
-            }
-            if(isMatch){
-                answer=1;
-                break;
+        // spell을 정렬하여 비교의 기준으로 사용
+        Arrays.sort(spell);
+        String spellKey = String.join("", spell);
+
+        for (String word : dic) {
+            // dic의 각 단어도 정렬하여 비교
+            char[] wordChars = word.toCharArray();
+            Arrays.sort(wordChars);
+            String sortedWord = new String(wordChars);
+
+            if (spellKey.equals(sortedWord)) {
+                return 1; // 일치하는 단어가 있으면 1 반환
             }
         }
-        return answer;
+
+        return 2; // 없으면 2 반환
     }
 }
