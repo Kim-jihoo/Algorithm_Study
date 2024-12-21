bacd4809e8226b62f4d09c294ec1c4d800bcf74b
Kim-jihoo <109203288+Kim-jihoo@users.noreply.github.com>
2024-12-22 03:43:43 +0900

    [level 1] Title: 폰켓몬, Time: 5.16 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,15 +1,15 @@
 import java.util.*;
 class Solution {
     public int solution(int[] nums) {
+        int answer = 0;
+        int n = nums.length/2;
         HashSet<Integer> set = new HashSet<>();
-        for(int i=0; i<nums.length;i++){
+        for(int i=0; i<nums.length; i++){
             set.add(nums[i]);
         }
-        int a = nums.length/2;
-        int answer = 0;
-        if(set.size()>a){
-            answer=a;
-        }else answer=set.size();
+       if(set.size()>n){
+           answer=n;
+       }else answer=set.size();
         return answer;
     }
 }
