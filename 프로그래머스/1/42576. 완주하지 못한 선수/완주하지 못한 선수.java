5b00546a2bcb7938175810524e7e19f8329a4ed8
Kim-jihoo <109203288+Kim-jihoo@users.noreply.github.com>
2024-12-23 03:58:46 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 127.30 ms, Memory: 98.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,19 +1,21 @@
 import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        
+        String answer = "";
         HashMap<String, Integer> map = new HashMap<>();
-        for(String player : participant){
-            map.put(player,map.getOrDefault(player,0)+1);
+        for(int i=0; i<participant.length;i++){
+            map.put(participant[i],map.getOrDefault(participant[i],0)+1);
         }
-        for(String player : completion){
-            map.put(player, map.get(player)-1);
+        for(int i=0; i<completion.length; i++){
+            if(map.containsKey(completion[i])){
+                map.put(completion[i],map.get(completion[i])-1);
+            }
         }
         for(String key : map.keySet()){
             if(map.get(key)!=0){
-                return key;
+                answer=key;
             }
         }
-        return "";
+        return answer;
     }
 }
