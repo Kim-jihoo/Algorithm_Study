d9ff4141fc40f763d900b7c83f6e41b5c6e6bd20
Kim-jihoo <109203288+Kim-jihoo@users.noreply.github.com>
2024-12-22 18:58:22 +0900

    [level 1] Title: 옹알이 (2), Time: 1.01 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,23 +1,22 @@
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
-        
-        
-        for(int i=0; i<babbling.length; i++){
-            String word = babbling[i];
-             if (word.contains("ayaaya") || word.contains("yeye") || word.contains("woowoo") || word.contains("mama")) {
+
+        for (String word : babbling) {
+            if (word.contains("ayaaya") || word.contains("yeye") || word.contains("woowoo") || word.contains("mama")) {
                 continue;
             }
+
             word = word.replace("aya", " ");
             word = word.replace("ye", " ");
             word = word.replace("woo", " ");
             word = word.replace("ma", " ");
-            word = word.replace(" ","");
+            word = word.replace(" ", "");
             if (word.equals("")) {
                 answer++;
             }
         }
-        
+
         return answer;
     }
 }
