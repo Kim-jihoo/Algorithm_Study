da611cc841e5275d5b1d483d240703c8b4b724d3
Kim-jihoo <109203288+Kim-jihoo@users.noreply.github.com>
2024-12-23 21:32:23 +0900

    [level 2] Title: 짝지어 제거하기, Time: 261.03 ms, Memory: 130 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution
+{
+    public int solution(String s)
+    {
+        int answer = 0;
+        Stack<String> stack = new Stack<>();
+        String[] str = s.split("");
+        for(int i=0; i<str.length; i++){
+            if(!stack.isEmpty()&&stack.peek().equals(str[i])){
+                stack.pop();
+            }else{
+                stack.push(str[i]);
+            }
+        }
+        if(stack.isEmpty()){
+            answer=1;
+        }
+
+        return answer;
+    }
+}
