9507c50d2dacc9fe50fe5df62ee42ea739aaceca
Kim-jihoo <109203288+Kim-jihoo@users.noreply.github.com>
2024-12-26 15:28:00 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.03 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,34 +1,19 @@
 class Solution {
     public int[] solution(int n, int m) {
         int[] answer = new int[2];
-        answer[0] = max(n,m);
-        answer[1]=min(n,m);
+        answer[0]=gcd(n,m);
+        answer[1]=lcd(answer[0],n,m);
         return answer;
     }
-    public int max(int n, int m){
-        int a =1;
-        int b =1;
-        if(n>m) b=m;
-        else b=n;
-        for(int i=2; i<=b; i++){
-            if(n%i==0&&m%i==0){
-                a=i;
-            }
+    public int gcd(int n, int m){
+        while(m!=0){
+            int temp = m;
+            m =n%m;
+            n=temp;
         }
-        return a;
+        return n;
     }
-    public int min(int n, int m){
-        int a = n*m;
-        if(n>m){
-            int k = n;
-            n=m;
-            m=k;
-        }
-        for(int i=m; i>=1; i--){
-            if(n*i%m==0){
-                a=i*n;
-            }
-        }
-        return a;
+    public int lcd(int a, int n, int m){
+        return a*(n/a)*(m/a);
     }
 }
