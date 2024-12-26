fa1f4ef69a36f60fd47962df248eaa3b453947ec
Kim-jihoo <109203288+Kim-jihoo@users.noreply.github.com>
2024-12-26 15:43:53 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 3.00 ms, Memory: 90.8 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -7,7 +7,6 @@ class Solution {
         
         for(int i = 0; i < arr.length; i++) {
             
-            // 문자열의 길이가 0이라면 공백 추가
             if(arr[i].length() == 0) answer += " "; 
             
             else {
@@ -18,10 +17,8 @@ class Solution {
             
         }
         
-        // 입력 받은 문자열의 맨 마지막이 " " 라면 바로 answer 반환
         if(s.substring(s.length() -1, s.length()).equals(" ")) return answer;
         
-        // 맨 마지막 " " 제거하고 answer 반환
         return answer.substring(0, answer.length() - 1);
         
     }
