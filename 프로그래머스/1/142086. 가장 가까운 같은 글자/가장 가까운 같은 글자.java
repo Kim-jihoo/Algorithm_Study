bb45e990b797f12f90a6a0edd7ad0b58c8bc2fda
Kim-jihoo <109203288+Kim-jihoo@users.noreply.github.com>
2024-12-26 21:28:27 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 378.21 ms, Memory: 91.4 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -1,13 +1,14 @@
 class Solution {
     public int[] solution(String s) {
         int[] answer = new int[s.length()];
-        String[] str = s.split("");
-        char[] ch = new char[s.length()];
-        for(int i=0; i<s.length(); i++){
+        String [] str = s.split("");
+        answer[0]=-1;
+        for(int i=1; i<s.length();i++){
             answer[i]=-1;
-            for(int j=0; j<=i-1; j++){
+            for(int j=0; j<i; j++){
                 if(str[i].equals(str[j])){
                     answer[i]=i-j;
+                    
                 }
             }
         }
