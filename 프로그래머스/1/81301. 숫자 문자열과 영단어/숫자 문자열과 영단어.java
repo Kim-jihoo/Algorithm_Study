7843aff242270eb8fcc6e1608970131b0b3bec74
Kim-jihoo <109203288+Kim-jihoo@users.noreply.github.com>
2024-12-29 16:50:02 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.09 ms, Memory: 90.9 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,48 +1,18 @@
 class Solution {
     public int solution(String s) {
-        StringBuilder answer = new StringBuilder();
-        StringBuilder sb = new StringBuilder();
-        for(int i=0; i<s.length(); i++){
-            char ch = s.charAt(i);
-            
-            if(ch>='0'&&ch<='9'){
-                answer.append(ch);
-            }else{
-                sb.append(ch);
-                String temp = sb.toString();
-                if (temp.equals("zero")) {
-                    answer.append("0");
-                    sb.delete(0, sb.length());
-                } else if (temp.equals("one")) {
-                    answer.append("1");
-                    sb.delete(0, sb.length());
-                } else if (temp.equals("two")) {
-                    answer.append("2");
-                    sb.delete(0, sb.length());
-                } else if (temp.equals("three")) {
-                    answer.append("3");
-                    sb.delete(0, sb.length());
-                } else if (temp.equals("four")) {
-                    answer.append("4");
-                    sb.delete(0, sb.length());
-                } else if (temp.equals("five")) {
-                    answer.append("5");
-                    sb.delete(0, sb.length());
-                } else if (temp.equals("six")) {
-                    answer.append("6");
-                    sb.delete(0, sb.length());
-                } else if (temp.equals("seven")) {
-                    answer.append("7");
-                    sb.delete(0, sb.length());
-                } else if (temp.equals("eight")) {
-                    answer.append("8");
-                    sb.delete(0, sb.length());
-                } else if (temp.equals("nine")) {
-                    answer.append("9");
-                    sb.delete(0, sb.length());
-                }
-            }
-        }
-        return Integer.parseInt(answer.toString());
+        int answer = 0;
+        String str = s;
+        str = str.replace("zero","0");
+        str = str.replace("one","1");
+        str = str.replace("two","2");
+        str = str.replace("three","3");
+        str = str.replace("four","4");
+        str = str.replace("five","5");
+        str = str.replace("six","6");
+        str = str.replace("seven","7");
+        str = str.replace("eight","8");
+        str = str.replace("nine","9");
+        answer = Integer.parseInt(str);
+        return answer;
     }
 }
